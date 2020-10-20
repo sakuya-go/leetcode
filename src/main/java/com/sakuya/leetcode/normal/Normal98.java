cd1f5ac6e422675c19f76b0046f09e79e1ef888d
huangwenkai <304657792@qq.com>
2020-10-20 12:00:02 +0800

    上传每日一题 Normal70, Normal66

---
+++ src/main/java/com/sakuya/leetcode/normal/Normal98.java
@@ -0,0 +1,112 @@
+package com.sakuya.leetcode.normal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 假设一个二叉搜索树具有如下特征：
+ * <p>
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * 2
+ * / \
+ * 1   3
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:
+ * 5
+ * / \
+ * 1   4
+ *      / \
+ *     3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ *      根节点的值为 5 ，但是其右子节点值为 4 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Normal98 {
+    public boolean isValidBST(TreeNode root) {
+        inOrderTraversal(root);
+        if (list.size() <= 1) return true;
+        System.out.println(list);
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) > list.get(i + i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> inOrderTraversal(TreeNode root) {
+        if (root != null) {
+            inOrderTraversal(root.left);
+            list.add(root.val);
+            inOrderTraversal(root.right);
+        }
+        return list;
+    }
+
+    /**
+     * 官方答案1 递归
+     */
+    public boolean isValidBST_1(TreeNode root) {
+        return helper(root, null, null);
+    }
+
+    public boolean helper(TreeNode node, Integer lower, Integer upper) {
+        if (node == null) return true;
+
+        int val = node.val;
+        if (lower != null && val <= lower) return false;
+        if (upper != null && val >= upper) return false;
+
+        if (!helper(node.right, val, upper)) return false;
+        if (!helper(node.left, lower, val)) return false;
+        return true;
+    }
+
+    /**
+     * 官方答案2 利用中序遍历
+     */
+    public boolean isValidBST_2(TreeNode root) {
+        Stack<TreeNode> stack = new Stack();
+        double inorder = -Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            // 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
+            if (root.val <= inorder) return false;
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
+
