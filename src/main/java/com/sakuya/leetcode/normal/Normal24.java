7821a31385fa22bc63e55ec5ea609d2279f4748a
huangwenkai <304657792@qq.com>
2020-10-22 21:01:35 +0800

    上传每日一题 Normal24

---
+++ src/main/java/com/sakuya/leetcode/normal/Normal24.java
@@ -0,0 +1,77 @@
+package com.sakuya.leetcode.normal;
+
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * <p>
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * 示例 2：
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：head = [1]
+ * 输出：[1]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Normal24 {
+
+    /**
+     * 递归解法
+     */
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+
+    /**
+     * 哨兵解法
+     */
+    public ListNode _swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead;
+        dummyHead.next = head;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
